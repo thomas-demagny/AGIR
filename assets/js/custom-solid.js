3bf27e6a546af00c995415a575e882c45cb4131a
Thomas Dem <demagny.t@hotmail.fr>
2020-01-23 14:54:33 +0100

    indent

--- assets/js/custom-solid.js
+++ assets/js/custom-solid.js
@@ -15,83 +15,72 @@
 
 $(document).ready(function () {
 
-/*====================================
-SCROLLING SCRIPTS
-======================================*/
+    /*====================================
+    SCROLLING SCRIPTS
+    ======================================*/
 
-$('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
-var $anchor = $(this);
-$('html, body').stop().animate({
-scrollTop: $($anchor.attr('href')).offset().top
-}, 1200, 'easeInOutExpo');
-event.preventDefault();
-});
-
-
-/*====================================
-SLIDER SCRIPTS
-======================================*/
+    $('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1200, 'easeInOutExpo');
+        event.preventDefault();
+    });
 
 
-$('#carousel-slider').carousel({
-interval: 6000 //MILLI SECONDS
-});
+    /*====================================
+    SLIDER SCRIPTS
+    ======================================*/
 
 
-/*====================================
-POPUP IMAGE SCRIPTS
-======================================*/
-$('.fancybox-media').fancybox({
-openEffect: 'elastic',
-closeEffect: 'elastic',
-helpers: {
-title: {
-type: 'inside'
-}
-}
-});
+    $('#carousel-slider').carousel({
+        interval: 6000 //MILLI SECONDS
+    });
 
 
-/*====================================
-FILTER FUNCTIONALITY SCRIPTS
-======================================*/
-$(window).load(function () {
-var $container = $('#work-div');
-$container.isotope({
-filter: '*',
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-$('.categories a').click(function () {
-$('.categories .active').removeClass('active');
-$(this).addClass('active');
-var selector = $(this).attr('data-filter');
-$container.isotope({
-filter: selector,
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-return false;
-});
-
-});
+    /*====================================
+    POPUP IMAGE SCRIPTS
+    ======================================*/
+    $('.fancybox-media').fancybox({
+        openEffect: 'elastic',
+        closeEffect: 'elastic',
+        helpers: {
+            title: {
+                type: 'inside'
+            }
+        }
+    });
 
 
+    /*====================================
+    FILTER FUNCTIONALITY SCRIPTS
+    ======================================*/
+    $(window).load(function () {
+        var $container = $('#work-div');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.categories a').click(function () {
+            $('.categories .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
 
-/*====================================
-CUSTOM SCRIPTS
-======================================*/
-
-
-
-
-
+    });
 
 
 });
