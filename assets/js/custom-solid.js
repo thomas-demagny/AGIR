f19c5cdcdc8a509795963ccbd248a03ee33fa897
Thomas Dem <demagny.t@hotmail.fr>
2020-01-27 14:46:24 +0100

    added scroll top

--- assets/js/custom-solid.js
+++ assets/js/custom-solid.js
@@ -1,31 +1,32 @@
-/*=============================================================
-    Authour URL: www.designbootstrap.com
-    
-    http://www.designbootstrap.com/
 
-    License: MIT
-
-    http://opensource.org/licenses/MIT
-
-    100% Free To use For Personal And Commercial Use.
-
-    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
-   
-========================================================  */
 
 $(document).ready(function () {
 
     /*====================================
     SCROLLING SCRIPTS
     ======================================*/
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.scroll-top').fadeIn();
+        } else {
+            $('.scroll-top').fadeOut();
+        }
+    });
 
-    $('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
+    $('.scroll-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 100);
+        return false;
+    });
+
+    /*$('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1200, 'easeInOutExpo');
         event.preventDefault();
-    });
+    });*/
 
 
     /*====================================
@@ -65,22 +66,14 @@ $(document).ready(function () {
                 queue: false
             }
         });
-        $('.categories a').click(function () {
-            $('.categories .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
+
 
     });
 
-
 });
+
+
+
+
+
+
